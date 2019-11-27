59876b1deaaa69e0e46d3d4ff7022108e938ff73
drdima2 <drdima2@gmail.com>
2019-11-27 13:27:10 -0600

    preparation for heroku deployment

--- src/main/java/ca/costsaver/listener/CostSaverApplicationListener.java
+++ src/main/java/ca/costsaver/listener/CostSaverApplicationListener.java
@@ -1,10 +1,15 @@
 package ca.costsaver.listener;
 
 import org.slf4j.Logger;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -17,6 +22,21 @@ public class CostSaverApplicationListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         log.info("CostSaver application Initialized");
         //ServiceManager.getInstance(sce.getServletContext());
+
+        //URI url = WebApplicationContextUtils.getRequiredWebApplicationContext(sce.getServletContext()).getBean("dbUrl",URI.class);
+        DriverManagerDataSource dataSource = WebApplicationContextUtils.getRequiredWebApplicationContext(sce.getServletContext()).getBean("dataSource",DriverManagerDataSource.class);
+        String herokuDB = System.getenv("DATABASE_URL");
+        //herokuDB = "postgresql://costsaver:password@localhost:5432/costsaver";
+        if (herokuDB!=null){
+            try {
+                URI dbUrl = new URI(herokuDB);
+                dataSource.setUrl("jdbc:postgresql://" + dbUrl.getHost() + ':' + dbUrl.getPort() + dbUrl.getPath());
+                dataSource.setUsername(dbUrl.getUserInfo().split(":")[0] );
+                dataSource.setPassword(dbUrl.getUserInfo().split(":")[1] );
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
---
+++ src/main/java/ca/costsaver/main/PropTest.java
@@ -0,0 +1,35 @@
+package ca.costsaver.main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
+public class PropTest {
+
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+        PropTest propTest = new PropTest();
+
+        Properties properties = new Properties();
+        properties.load((propTest.getClass().getClassLoader().getResourceAsStream("application.properties")));
+
+        URI dbUrl = new URI(properties.getProperty("db.url"));
+
+
+        System.out.println(dbUrl.getHost());
+        System.out.println(dbUrl.getPort());
+        System.out.println(dbUrl.getPath());
+
+        System.out.println(dbUrl.getUserInfo());
+        System.out.println(dbUrl.getUserInfo().split(":")[0]);
+        System.out.println(dbUrl.getUserInfo().split(":")[1]);
+
+
+    }
+
+}
